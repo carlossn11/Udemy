9d7dda43c34b8728aff1e60107154527257f1d0e
Carlos Aguilera <ca@Carloss-MacBook-Air.local>
2021-12-26 08:11:57 -0800

    working on ticket

--- Functions/src/func.cpp
+++ Functions/src/func.cpp
@@ -3,4 +3,6 @@
 void coffee(double coffeePrice, double salesTax, double coffeeCupPrice)
 {
     std::cout << ((coffeePrice + coffeeCupPrice) * salesTax) + coffeeCupPrice + coffeePrice << "\n";
+    //this is a test for a new branch
+    
 }
