c7ed46d1384f21ccaf99464d1f0433ad8dbc68f1
Carlos Aguilera <18aguilerac@gmail.com>
2022-05-03 18:10:37 -0700

    completed challenge for section 16

---
+++ Section16/Challenge/include/I_Printable.h
@@ -0,0 +1,11 @@
+#ifndef I_PRINTABLE_H_
+#define I_PRINTABLE_H_
+#include <iostream>
+
+class I_Printable {
+  friend std::ostream &operator<<(std::ostream &os, const I_Printable &obj);
+public:
+  virtual void print(std::ostream &os) const = 0;
+};
+
+#endif // I_PRINTABLE_H_
--- Section16/Challenge/src/main.cpp
+++ Section16/Challenge/src/main.cpp
@@ -1,3 +1,5 @@
+// Section 16 
+// Challenge 
 #include <iostream>
 #include <vector>
 #include "../include/Account.h"
@@ -6,7 +8,29 @@
 #include "../include/Trust_Account.h"
 #include "../include/Account_Util.h"
 
+using namespace std; 
+
 int main() {
-  return 0;
+    cout.precision(2);
+    cout << fixed;
+   
+    vector<Account*> accounts;
+    accounts.push_back(new Savings_Account {} );
+    accounts.push_back(new Savings_Account {"Superman"} );
+    accounts.push_back(new Savings_Account {"Batman", 2000} );
+    accounts.push_back(new Savings_Account {"Wonderwoman", 5000, 5.0} );
+
+    accounts.push_back(new Checking_Account {} );
+    accounts.push_back(new Checking_Account {"Kirk"} );
+    accounts.push_back(new Checking_Account {"Spock", 2000} );
+    accounts.push_back(new Checking_Account {"Bones", 5000} );
+
+    accounts.push_back(new Trust_Account {} );
+    accounts.push_back(new Trust_Account {"Athos", 10000, 5.0} );
+    accounts.push_back(new Trust_Account {"Porthos", 20000, 4.0} );
+    accounts.push_back(new Trust_Account {"Aramis", 30000} );
+
+    display(accounts);
+    return 0;
 }
 
