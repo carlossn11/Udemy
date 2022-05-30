a10776cfc37f848f584f04d7f2ec2d2896225201
Carlos Aguilera <18aguilerac@gmail.com>
2022-05-29 20:09:59 -0700

    Section 20

--- Iterators/src/main.cpp
+++ Iterators/src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <list>
+#include <iterator>
  
 int main() {
-  std::cout << "Hello World!";
+  std::vector<int> vec { 1, 3, 4, 6 };
+  std::vector<int>::iterator it = vec.end();
+  std::cout << it;
   return 0;
 }
