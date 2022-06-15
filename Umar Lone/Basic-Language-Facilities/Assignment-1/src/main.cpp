49ead8e85bd6ae8352d2cb1428a8fa729dc37717
Carlos Aguilera <18aguilerac@gmail.com>
2022-06-14 22:46:35 -0700

    Swap Function

--- Umar Lone/Basic-Language-Facilities/Assignment-1/src/main.cpp
+++ Umar Lone/Basic-Language-Facilities/Assignment-1/src/main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <iomanip>
 
 int add(int* a, int* b);
+void swap(int* a, int* b);
 
 int main() {
-  int a = 10;
-  int b = 10;
-  int result = 0;
-  result = add(&a, &b);
-  std::cout << result;
+  int a = 1;
+  int b = 5;
+  std::cout << std::left;
+  std::cout << std::setw(2) << a << b << '\n';
+  swap(&a, &b);
+  std::cout << std::setw(2) << a << b << '\n';
+  std::cout << std::right;
+
   return 0;
 }
 
 int add(int* a, int* b) {
   return (*a + *b);
+}
+
+void swap(int* a, int* b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
 }
