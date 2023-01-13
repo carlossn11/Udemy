7acd6dd4322a62602382744756832b083eb65458
Carlos Aguilera <18aguilerac@gmail.com>
2023-01-12 21:26:42 -0800

    quick challenge

--- Frank Mitropolus/Section20/Challenge_1/src/main.cpp
+++ Frank Mitropolus/Section20/Challenge_1/src/main.cpp
@@ -9,36 +9,39 @@
 #include <vector>
 #include <iomanip>
 
-bool is_palindrome(const std::string& s) {
-  std::deque<char> dequeTypeChar;
-  std::copy_if(s.cbegin(), s.cend(), std::back_inserter(dequeTypeChar), 
-  [] (char c) {
-    if (std::isalpha(c))
-      return true;
+bool is_palindrome(const std::string &s)
+{
+  std::deque<char> cleaned;
+  std::copy_if(s.begin(), s.end(), std::back_inserter(cleaned), [](const char &c)
+               { return std::isalpha(c); });
 
-    return false;
-  });
+  std::deque<char>::const_iterator left = cleaned.begin();
+  auto right = cleaned.cend() - 1;
 
-  std::transform(std::cbegin(dequeTypeChar), std::cend(dequeTypeChar), std::begin(dequeTypeChar), ::toupper);
-
-  std::deque<char> reversedTypeChar;
-  std::copy(dequeTypeChar.crbegin(), dequeTypeChar.crend(), std::back_inserter(reversedTypeChar));
-
-  if (dequeTypeChar == reversedTypeChar)
-    return true;
-
-  return false;
+  while (left != cleaned.end())
+  {
+    if (std::tolower(*left) != std::tolower(*right))
+    {
+      return false;
+    }
+    ++left;
+    --right;
+  }
+  return true;
 }
 
-int main() {
-  std::vector<std::string> test_strings { "a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana",
-    "avid diva", "Amore, Roma", "A Toyota's a toyota", "A Santa at NASA", "C++",
-    "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", "This is a palindrome", "palindrome" };
-  
+int main()
+{
+  std::vector<std::string> test_strings{"a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana",
+                                        "avid diva", "Amore, Roma", "A Toyota's a toyota", "A Santa at NASA", "C++",
+                                        "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", "This is a palindrome", "palindrome"};
+
   std::cout << std::boolalpha;
-  std::cout << std::setw(8) << std::left << "Result" << "String" << std::endl;
-  for(const auto& s : test_strings) {
-    std::cout << std::setw(8) << std::left << is_palindrome(s)  << s << std::endl;
+  std::cout << std::setw(8) << std::left << "Result"
+            << "String" << std::endl;
+  for (const auto &s : test_strings)
+  {
+    std::cout << std::setw(8) << std::left << is_palindrome(s) << "          " << s << std::endl;
   }
   std::cout << std::endl;
   return 0;
