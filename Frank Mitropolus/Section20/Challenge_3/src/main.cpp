704e5393ce884833594d78730a76073cd3167825
Carlos Aguilera <18aguilerac@gmail.com>
2023-01-18 22:37:19 -0800

    slider

--- Frank Mitropolus/Section20/Challenge_3/src/main.cpp
+++ Frank Mitropolus/Section20/Challenge_3/src/main.cpp
@@ -1,5 +1,5 @@
 // Section 20
-// Challenge 3  
+// Challenge 3
 // Maps and Sets
 
 #include <iostream>
@@ -11,9 +11,11 @@
 #include <string>
 #include <iomanip>
 
-void display_words(const std::map<std::string, int> &words) {
+void display_words(const std::map<std::string, int> &words)
+{
     std::cout << std::left;
-    for (auto it = words.cbegin(); it != words.cend(); ++it) {
+    for (auto it = words.cbegin(); it != words.cend(); ++it)
+    {
         std::cout << std::setw(15) << it->first << it->second << std::endl;
     }
     std::cout << std::right;
@@ -21,23 +23,26 @@ void display_words(const std::map<std::string, int> &words) {
 
 void display_words(const std::map<std::string, std::set<int>> &words)
 {
-     std::cout << std::setw(12) << std::left << "\nWord"
-                << "Occurrences"<< std::endl;
+    std::cout << std::setw(12) << std::left << "\nWord"
+              << "Occurrences" << std::endl;
     std::cout << "=====================================================================" << std::endl;
-    for (auto pair: words) {
-        std::cout << std::setw(12) << std::left << pair.first 
-                       << std::left << "[ ";
-        for (auto i: pair.second) 
+    for (auto pair : words)
+    {
+        std::cout << std::setw(12) << std::left << pair.first
+                  << std::left << "[ ";
+        for (auto i : pair.second)
             std::cout << i << " ";
         std::cout << "]" << std::endl;
     }
 }
 
-// This function removes periods, commas, semicolons and colon in 
+// This function removes periods, commas, semicolons and colon in
 // a string and returns the clean version
-std::string clean_string(const std::string &s) {
+std::string clean_string(const std::string &s)
+{
     std::string result;
-    for (char c: s) {
+    for (char c : s)
+    {
         if (c == '.' || c == ',' || c == ';' || c == ':')
             continue;
         else
@@ -47,51 +52,60 @@ std::string clean_string(const std::string &s) {
     return result;
 }
 
-// Part1 process the file and builds a map of words and the 
+// Part1 process the file and builds a map of words and the
 // number of times they occur in the file
 
-void part1() {
-    std::map<std::string, int> words;
-    std::string word;   
-    std::ifstream in_file {"words.txt"};
-    if (in_file) {
-        while (in_file >> word) {
-            word = clean_string(word);
-            words[word] += 1;
-        }
-        in_file.close();
-        display_words(words);
-    } else {
-        std::cerr << "Error opening input file" << std::endl;
-    }
-}
-    
-// Part2 process the file and builds a map of words and a 
-// set of line numbers in which the word appears
-void part2() {
-    std::map<std::string, std::set<int>> words;
-    std::string line;
-    int lineCount = 1;
+void part1()
+{
+    std::fstream fin("../words.txt", std::ios::in);
+
     std::string word;
-    std::ifstream in_file {"words.txt"};
-    if (in_file) {
-        while (std::getline(in_file, line)) {
-            std::stringstream ss(line);
-            while (ss >> word) {
-                word = clean_string(word);
-                words[word].insert(lineCount);
-            }
-            ++lineCount;
-        }
-        display_words(words);
-        in_file.close();
-    } else {
-        std::cerr << "Error opening input file" << std::endl;
+    std::map<std::string, int> map;
+
+    while (!fin.eof())
+    {
+        fin >> word;
+        word = clean_string(word);
+        map[word] += 1;
     }
+
+    fin.close();
+
+    display_words(map);
 }
 
-int main() {
+// Part2 process the file and builds a map of words and a
+// set of line numbers in which the word appears
+void part2()
+{
+    std::fstream fin("../words.txt", std::ios::in);
+
+    int nLine = 1;
+    std::string s;
+    std::map<std::string, std::set<int>> map;
+
+    while (!fin.eof())
+    {
+        std::getline(fin, s);
+        std::stringstream strLine(s);
+
+        while (!strLine.eof())
+        {
+            strLine >> s;
+            s = clean_string(s);
+            map[s].insert(nLine);
+        }
+
+        ++nLine;
+    }
+
+    fin.close();
+
+    display_words(map);
+}
+
+int main()
+{
     part2();
     return 0;
 }
-
